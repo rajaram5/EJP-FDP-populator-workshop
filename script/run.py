49ec64f721c6c1d5ee4efee80eddee12d099aa5c
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-03-11 10:46:49 +0100

    query has been modified

--- script/run.py
+++ script/run.py
@@ -1,7 +1,7 @@
 import FdpPopulator
 
-fdp_url = "http://localhost"
-#fdp_url = "http://ejprd.fair-dtls.surf-hosted.nl:8094"
+#fdp_url = "http://localhost"
+fdp_url = "http://ejprd.fair-dtls.surf-hosted.nl:8094"
 fdp_admin_user = "albert.einstein@example.com"
 fdp_admin_password = "password"
 input_file = "/home/rajaram/Downloads/FDP-template.xlsx"
